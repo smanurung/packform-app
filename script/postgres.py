1a9fc104412e892b66594f99d8039786d667e0df
smanurung <sonthehottest@gmail.com>
2020-07-17 14:45:49 +1000

    data + postgreSQL script

---
+++ script/postgres.py
@@ -0,0 +1,50 @@
+import psycopg2
+import csv
+from datetime import datetime
+
+# This assumes database and tables are created.
+
+if __name__ == "__main__":
+    try:
+        conn = psycopg2.connect("dbname='packform-db' user='pack_admin' host='localhost' password='packpass'")
+    except Exception as e:
+        print "unable to connect to database {}".format(e)
+
+    cur = conn.cursor()
+
+    tasks = {}
+    tasks["delivery"] = {
+        "csv_name": "../data/pg_deliveries.csv",
+        "cmdfunc": lambda row: "insert into delivery (id,order_item_id,delivered_quantity) values ({},{},{})".format(int(row[0]), int(row[1]), int(row[2]))
+    }
+    tasks["order_items"] = {
+        "csv_name": "../data/pg_order_items.csv",
+        "cmdfunc": lambda row: "insert into order_items (id,order_id,price_per_unit,quantity,product) values ({},{},{},{},'{}')".format(int(row[0]), int(row[1]), float(row[2]), int(row[3]), row[4])
+    }
+    tasks["orders"] = {
+        "csv_name": "../data/pg_orders.csv",
+        "cmdfunc": lambda row: "insert into orders (id,created_at,order_name,customer_id) values ({},'{}','{}','{}')".format(int(row[0]), datetime.strptime(row[1], '%Y-%m-%dT%H:%M:%SZ'), row[2], row[3])
+    }
+
+    for k, v in tasks.items():
+        # print(k, v)
+        with open(v['csv_name']) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1 # skip header
+                    continue
+                print(row)
+                cmd = v['cmdfunc'](row)
+                try:
+                    cur.execute(cmd)
+                except Exception as e:
+                    print("unable to execute {}: {}".format(cmd, e))
+                print("success exec: {}".format(cmd))
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+    print "ok"
---
+++ script/postgres.sql
@@ -0,0 +1,25 @@
+create user pack_admin;
+create database packform-db;
+grant all privileges on database packform-db to pack_admin;
+alter user pack_admin with encrypted password 'packpass';
+
+create table delivery (
+    id serial primary key,
+    order_item_id integer not null,
+    delivered_quantity integer not null
+);
+
+create table order_items (
+    id serial primary key,
+    order_id integer not null,
+    price_per_unit real not null,
+    quantity integer not null,
+    product varchar(128) not null
+);
+
+create table orders (
+    id serial primary key,
+    created_at timestamp not null,
+    order_name varchar(128) not null,
+    customer_id varchar(128) not null
+);
